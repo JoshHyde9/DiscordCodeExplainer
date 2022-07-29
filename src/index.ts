fc35ff445bb439321467355501f14e33ca5e8fbc
JoshHyde9 <joshhyde546@gmail.com>
2022-07-29 14:04:01 +1000

    This is all I can do for now

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ActivityType, Client } from "discord.js";
+import { ActivityType, Client, EmbedBuilder } from "discord.js";
 import { Configuration, OpenAIApi } from "openai";
 import "dotenv/config";
 
@@ -9,7 +9,7 @@ const client = new Client({
 
 // Config Open AI
 const config = new Configuration({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey: process.env.OPEN_AI_KEY as string,
 });
 
 const openai = new OpenAIApi(config);
@@ -34,7 +34,24 @@ client.once("ready", () => {
 });
 
 // Send a request to the OpenAI API
-const analyseCode = (code: string) => {};
+const analyseCode = async (code: string) => {
+  const { data } = await openai.createCompletion({
+    model: "code-davinci:002",
+    prompt: code,
+    temperature: 0,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ['"""'],
+  });
+
+  if (data.choices === undefined || data.choices[0].text === "") {
+    return "Couldn't find anything...";
+  }
+
+  return data.choices[0].text;
+};
 
 // Everytime a message is created
 client.on("messageCreate", async (messageCreate) => {
@@ -57,6 +74,25 @@ client.on("messageCreate", async (messageCreate) => {
   // Remove code block tags
   message = message.replace(regExp, ""); // Remove what what the regex matched
   message = message.slice(0, -3); // Remove the ``` at the end of the code block
+
+  const code = JSON.stringify(message);
+
+  const data = await analyseCode(code);
+
+  if (!data) {
+    messageCreate.reply("Couldn't find anything...");
+    return;
+  }
+
+  const embed = new EmbedBuilder()
+    .setColor("#ff00ff")
+    .setTitle("Code Explainer")
+    .setDescription("This is my interpretation of your hot garbage code")
+    .addFields({ name: "Your code: ", value: data! })
+    .setTimestamp()
+    .setFooter({ text: "This was created using Open AI" });
+
+  messageCreate.reply({ embeds: [embed] });
 });
 
 // Login to Discord
