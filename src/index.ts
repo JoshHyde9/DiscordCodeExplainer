305c55a24c73b077806de7cf7f1d9b199746bcb0
JoshHyde9 <joshhyde546@gmail.com>
2022-07-29 12:58:51 +1000

    Remove code block from messages

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { ActivityType, Client } from "discord.js";
 import "dotenv/config";
 
 // Initialise a new client
-const client = new Client({ intents: ["Guilds"] });
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
 
 // When the bot is ready
 client.once("ready", () => {
@@ -21,6 +23,29 @@ client.once("ready", () => {
         : `${client.guilds.cache.size} servers`
     }`
   );
+});
+
+// Everytime a message is created
+client.on("messageCreate", async (messageCreate) => {
+  // Ignore the message if the message is sent from a bot
+  if (messageCreate.author.bot) return;
+
+  // Message content
+  let message = messageCreate.content.toString();
+
+  // Get the first 3 characters and check if the next two characters are alphabetical with an optional 3rd character
+  const regExp = new RegExp(/^```[a-z][a-z]?[a-z]?\n/);
+
+  const isCodeBlock = message.match(regExp);
+
+  // Do nothing if the message is not a code block or an invalid code block
+  if (!isCodeBlock) {
+    return;
+  }
+
+  // Remove code block tags
+  message = message.replace(regExp, ""); // Remove what what the regex matched
+  message = message.slice(0, -3); // Remove the ``` at the end of the code block
 });
 
 // Login to Discord
