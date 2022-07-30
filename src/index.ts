12ac948fdb480ff75eef8bdd3865ec89f9b30fda
JoshHyde9 <joshhyde546@gmail.com>
2022-07-30 11:02:30 +1000

    Added comments and moved some functions around

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { ActivityType, Client, EmbedBuilder } from "discord.js";
 import { Configuration, OpenAIApi } from "openai";
 import "dotenv/config";
 
-import { Dalle, Generations } from "./dalle";
+import { Dalle, ImageGenerations } from "./dalle";
 
 // Initialise a new client
 const client = new Client({
@@ -17,7 +17,7 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 // Config Dalle-2
-const dalle = new Dalle(process.env.OPEN_AI_SESS_KEY!);
+const dalle = new Dalle({ apiKey: process.env.OPEN_AI_SESS_KEY! });
 
 // Dalle-2 API
 const getDalleImage = async (caption: string) => {
@@ -26,22 +26,7 @@ const getDalleImage = async (caption: string) => {
   return imageGenerations;
 };
 
-// When the bot is ready
-client.once("ready", () => {
-  // Set the bot's status
-  client.user!.setPresence({
-    activities: [{ name: "Your garbage code", type: ActivityType.Watching }],
-    status: "idle",
-  });
-
-  // Log when bot turns on
-  console.log("Ready or not, here I come to look at some garbage code!");
-  console.log(
-    `I am currently connected to ${client.guilds.cache.size} ${client.guilds.cache.size > 1 ? "servers" : "server"}`
-  );
-});
-
-// Send a request to the OpenAI API
+// Send a request to the Codex Open AI API
 const analyseCode = async (code: string) => {
   const { data } = await openai.createCompletion({
     model: "code-davinci:002",
@@ -60,6 +45,23 @@ const analyseCode = async (code: string) => {
 
   return data.choices[0].text;
 };
+
+// When the bot is ready
+client.once("ready", () => {
+  // Set the bot's status
+  client.user!.setPresence({
+    activities: [{ name: "Your garbage code", type: ActivityType.Watching }],
+    status: "idle",
+  });
+
+  // Log when bot turns on
+  console.log("Ready or not, here I come to look at some garbage code!");
+  console.log(
+    `I am currently connected to ${client.guilds.cache.size} ${
+      client.guilds.cache.size > 1 ? "servers" : "server"
+    }`
+  );
+});
 
 // Everytime a message is created
 client.on("messageCreate", async (messageCreate) => {
@@ -83,15 +85,17 @@ client.on("messageCreate", async (messageCreate) => {
   message = message.replace(regExp, ""); // Remove what what the regex matched
   message = message.slice(0, -3); // Remove the ``` at the end of the code block
 
-  const code = JSON.stringify(message);
+  const code = JSON.stringify(message); // Stringify the message
 
-  const data = await analyseCode(code);
+  const data = await analyseCode(code); // Send stringified message to the Codex Open AI API
 
+  // If the API didn't return anything
   if (!data) {
     messageCreate.reply("Couldn't find anything...");
     return;
   }
 
+  // Build an embed and reply to the user
   const embed = new EmbedBuilder()
     .setColor("#ff00ff")
     .setTitle("Code Explainer")
@@ -109,10 +113,12 @@ client.on("interactionCreate", async (interaction) => {
 
   const { commandName } = interaction;
 
+  // Command names
   switch (commandName) {
     case "dalle":
-      const caption = interaction.options.getString("caption");
+      const caption = interaction.options.getString("caption"); // Get the input from caption
 
+      // If user didn't input a caption
       if (!caption) {
         interaction.reply(
           "Please input a caption so I can generate an image for you."
@@ -121,12 +127,13 @@ client.on("interactionCreate", async (interaction) => {
         return;
       }
 
-      await interaction.deferReply();
-      const { data }: Generations = await getDalleImage(caption);
+      await interaction.deferReply(); // Display "bot is thinking..." whilst the bot talks to the Dalle-2 Open AI API
 
-      const image = data[0].generation.image_path;
+      const { data }: ImageGenerations = await getDalleImage(caption); // Call the API to get the image
 
-      await interaction.editReply(image);
+      const image = data[0].generation.image_path; // Get the first image URL from the response
+
+      await interaction.editReply(image); // Edit the reply with the image
       break;
 
     default:
